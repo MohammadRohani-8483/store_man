0cd1f74de42f371a0c94584fdae64afdf356e2f0
MohammadRohani-8483 <mohammad.rohani.1002@gmail.com>
2024-05-15 11:37:21 +0330

    fetch post request for test backend

--- frontend-store-man/app/page.tsx
+++ frontend-store-man/app/page.tsx
@@ -1,4 +1,23 @@
-export default function Home() {
+export default async function Home() {
+  const newProduct = {
+    name: 'teblet',
+    price: 2000,
+    count: 80
+  }
+  const config = {
+    method: "POST",
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(newProduct)
+  }
+  try {
+    const res = await fetch('http://localhost:5555/products', config)
+    const data = await res.json()
+    console.log('data =>', data);
+  } catch (err) {
+    console.log('error =>', err);
+  }
   return (
     <main></main>
   );
